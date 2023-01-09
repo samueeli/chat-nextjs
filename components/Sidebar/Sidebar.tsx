3c065270dc7bf30609f4ae4f58597a62e11c585d
samueeli <samuli.salminen@raivo.io>
2023-01-09 12:25:43 +0200

    cleanup and added materialUI + icons

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Sidebar = () => {
+  return (
+    <Container>
+      <Header>Hello</Header>
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+
+const Header = styled.div`
+  color: red;
+`;
