0e77664341864b168f02f5bcca7ab0fd651c2216
samueeli <samuli.salminen@raivo.io>
2023-01-09 13:47:43 +0200

    added header, search and button to sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
-import { Chat, MoreVert } from '@mui/icons-material';
-import { Avatar } from '@mui/material';
+import { Chat, MoreVert, Search } from '@mui/icons-material';
+import { Avatar, Button, Icon, IconButton } from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -10,20 +10,71 @@ export const Sidebar = () => {
         <UserAvatar />
 
         <IconContainer>
-          <Chat />
-          <MoreVert />
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
         </IconContainer>
       </Header>
+
+      <SearchContainer>
+        <Search />
+        <SearchInput placeholder="Search chats" />
+      </SearchContainer>
+
+      <SidebarButton>Start a new chat</SidebarButton>
+
+      {/* list of conversations */}
     </Container>
   );
 };
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+  z-index: 1;
+`;
 
 const UserAvatar = styled(Avatar)`
-  margin: 10px;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
 `;
 
 const IconContainer = styled.div``;
+
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  flex: 1;
+  border: none;
+  outline-width: 0;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+  color: black;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
