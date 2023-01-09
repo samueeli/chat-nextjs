ff1d50af84a3d4bd1b7dbbfedb46b96516424483
samueeli <samuli.salminen@raivo.io>
2023-01-09 12:38:57 +0200

    added also @emotion/react

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,16 +1,29 @@
+import { Chat, MoreVert } from '@mui/icons-material';
+import { Avatar } from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
 
 export const Sidebar = () => {
   return (
     <Container>
-      <Header>Hello</Header>
+      <Header>
+        <UserAvatar />
+
+        <IconContainer>
+          <Chat />
+          <MoreVert />
+        </IconContainer>
+      </Header>
     </Container>
   );
 };
 
 const Container = styled.div``;
 
-const Header = styled.div`
-  color: red;
+const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)`
+  margin: 10px;
 `;
+
+const IconContainer = styled.div``;
